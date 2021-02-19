537445426bd162d6421f82e42e45b232628448c4
Van921 <741759859@qq.com>
2021-02-19 12:00:04 +0800

    0219

---
+++ protect0218/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,27 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpRequest;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+    public void run(){
+        try {
+            HttpRequest request = new HttpRequest(socket);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                socket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
